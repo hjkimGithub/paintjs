a847b228a4c5588ea7c37b506fa15c79c36ffc9f
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-03 13:38:33 +0900

    Filling mode

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementsByClassName("jsRange");
+const mode = document.getElementById("jsMode");
+
+const INITIAL_VARIABLE = "#2c2c2c"
 
 // canvas.width = 700;
 // canvas.height = 700;
@@ -9,10 +12,12 @@ const range = document.getElementsByClassName("jsRange");
 canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
 canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
-ctx.strokeStyle = "#2c2c2c"
-ctx.lineWidth = 2.5
+ctx.strokeStyle = INITIAL_VARIABLE;
+ctx.fillStyle = INITIAL_VARIABLE;
+ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -37,6 +42,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -44,11 +50,26 @@ function handleRangeChange(event) {
     ctx.lineWidth = size
 }
 
+function handleModeClick() {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill"
+    } else {
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
+function handleCanvasClick(event) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove)
     canvas.addEventListener("mousedown", startPainting)
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
+    canvas.addEventListener("click", handleCanvasClick)
 }
 
 // if(colors) {
@@ -57,4 +78,8 @@ Array.from(colors).forEach(color => color.addEventListener("click", handleColorC
 
 if(range) {
     range.addEventListener("input", handleRangeChange)
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeClick)
 }
