a565d1367aab0696c44f627158139fcb5239a1ce
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-03 13:15:38 +0900

    brush size, color click

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
-const canvas = document.getElementById("jsCanvas")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementsByClassName("jsRange");
+
+// canvas.width = 700;
+// canvas.height = 700;
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+ctx.strokeStyle = "#2c2c2c"
+ctx.lineWidth = 2.5
 
 let painting = false;
 
 function stopPainting() {
-    painting = false
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
 }
 
 function onMouseMove(event) {
-    const x = event.offsetX
-    const y = event.offsetY
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
-function onMouseDown(event) {
-    painting = true
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor
+    ctx.strokeStyle = color;
 }
 
-function onMouseUp(event) {
-
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size
 }
 
-if (casvas) {
+if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove)
-    canvas.addEventListener("mousedown", onMouseDown)
-    canvas.addEventListener("mouseup", onMouseUp)
+    canvas.addEventListener("mousedown", startPainting)
+    canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
+}
+
+// if(colors) {
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+// }
+
+if(range) {
+    range.addEventListener("input", handleRangeChange)
 }
