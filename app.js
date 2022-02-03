0e8c95430acb04b4cc49f417489934e064335f82
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-03 14:13:32 +0900

    download img

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
-const range = document.getElementsByClassName("jsRange");
+const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave")
 
-const INITIAL_VARIABLE = "#2c2c2c"
+const INITIAL_COLOR = "#2c2c2c"
 
 // canvas.width = 700;
 // canvas.height = 700;
@@ -12,8 +13,10 @@ const INITIAL_VARIABLE = "#2c2c2c"
 canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
 canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
-ctx.strokeStyle = INITIAL_VARIABLE;
-ctx.fillStyle = INITIAL_VARIABLE;
+ctx.fillStyle="white"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -40,7 +43,7 @@ function onMouseMove(event) {
 }
 
 function handleColorClick(event) {
-    const color = event.target.style.backgroundColor
+    const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
 }
@@ -60,8 +63,22 @@ function handleModeClick() {
     }
 }
 
-function handleCanvasClick(event) {
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+    }
+}
+
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick(event) {
+    const image = canvas.toDataURL("image/jpeg");   
+    const link = document.createElement("a");
+    link.href = image
+    link.download = "PaintJs[Export]"
+    link.click()
 }
 
 if (canvas) {
@@ -70,6 +87,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
     canvas.addEventListener("click", handleCanvasClick)
+    canvas.addEventListener("contextmenu", handleCM)// right click
 }
 
 // if(colors) {
@@ -82,4 +100,8 @@ if(range) {
 
 if(mode) {
     mode.addEventListener("click", handleModeClick)
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick)
 }
